c12d25123cf7ea19d19128e75c86d66af2a1bd12
GentleMizt <olamideiyanda18@gmail.com>
2022-10-16 20:35:21 +0100

    5th commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Js is working now')
+body = document.body;
+formDiv = document.querySelector('.form-div');
+form = document.querySelector('form');
+table = document.querySelector('table');
+tableRows = document.querySelectorAll('tr');
+tableHead = document.querySelector('th');
+tableData = document.querySelectorAll('td');
